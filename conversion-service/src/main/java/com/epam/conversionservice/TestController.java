ddd18cd640d8ec48f023286b1005c9624bbb57ae
AgataPochebut <pochebut.a@gmail.com>
2020-05-27 22:09:10 +0300

    feat: add changes

---
+++ conversion-service/src/main/java/com/epam/conversionservice/TestController.java
@@ -0,0 +1,18 @@
+package com.epam.conversionservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() throws IOException {
+        return "conversion test";
+    }
+
+}
