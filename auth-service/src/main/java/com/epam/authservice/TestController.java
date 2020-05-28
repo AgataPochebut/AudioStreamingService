5defde105d9f0a7bd1d9d267be3a389806be9f34
AgataPochebut <pochebut.a@gmail.com>
2020-05-29 01:17:49 +0300

    feat: add changes

--- auth-service/src/main/java/com/epam/authservice/TestController.java
+++ auth-service/src/main/java/com/epam/authservice/TestController.java
@@ -1,5 +1,6 @@
 package com.epam.authservice;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +11,14 @@ import java.io.IOException;
 @RequestMapping
 public class TestController {
 
+    @HystrixCommand(fallbackMethod = "reliable")
     @GetMapping("/test")
     public String test() throws IOException {
         return "auth test";
+    }
+
+    public String reliable() {
+        return "Cloud Native Java (O'Reilly)";
     }
 
 }
--- song-service/src/main/java/com/epam/songservice/feign/conversion/ConversionClient.java
+++ song-service/src/main/java/com/epam/songservice/feign/conversion/ConversionClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-@FeignClient(value = "conversion", url = "http://localhost:8084/conversion")
+@FeignClient(value = "conversion-service")
 public interface ConversionClient {
 
     @PostMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
